650227d91216fc6e17178da5c76d092d2a808b0d
arakis2 <51448379+arakis2@users.noreply.github.com>
2023-04-11 12:09:43 +0200

    mise en place des routes

--- helper.js
+++
@@ -1,9 +0,0 @@
-exports.success = (message, data) => {
-    return {message, data}
-}
-
-exports.getUniqueId = (pokemons) => {
-    const pokemonsIds = pokemons.map(pokemon => pokemon.id)
-    let maxId = pokemonsIds.reduce((a,b) => Math.max(a,b))
-    return ++maxId
-}
