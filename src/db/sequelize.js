c895cc4fb4eae34b3d11be6363d4855b8088334e
arakis2 <51448379+arakis2@users.noreply.github.com>
2023-04-12 17:36:48 +0200

    oubli

--- src/db/sequelize.js
+++ src/db/sequelize.js
@@ -9,7 +9,7 @@ if(process.env.NODE_ENV.trim() === 'production'){
   sequelize = new Sequelize('test', '34285JFDwmEM7Xa.root', 'emYE7iWqZ3DjiK7R', {
     host: 'gateway01.ap-southeast-1.prod.aws.tidbcloud.com',
     port: 4000,
-    connectTimeout: 30000
+    connectTimeout: 30000,
     ssl: {
     minVersion: 'TLSv1.2',
     rejectUnauthorized: true
